03bdf7bd2c84d58446a61e37037465c0aa3424c7
Paco Sanchez <francisco.sanchez.14@gmail.com>
2017-11-08 20:24:20 +0100

    Removed node_modules

--- app_client/check_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    $outp = "";
-    if( isset($_SESSION['uid']) && isset($_SESSION['user'])){   
-        $outp .= '{"user":"'  . $_SESSION['user'] . '",';
-        $outp .= '"rol":"'. $_SESSION['rol']  . '"}';
-    }
-    echo $outp;
-?>
--- app_client/conexion.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$conn = mysqli_connect("localhost","root","","futurecharge") or die ("Error al conectar");
-    mysqli_set_charset($conn,"utf8");
-?>

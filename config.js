6bba295129f63216ae9f6c4a1b57aad56b828c7a
Paco Sanchez <francisco.sanchez.14@gmail.com>
2017-11-18 11:30:16 +0100

    updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: /*process.env.PORT ||*/ 6500,
+  port: process.env.PORT || 6500,
   db: process.env.MONGODB || /*'mongodb://localhost:27017/shop'*/'mongodb://paco.dev:PaCoSaNcHeZ1994@ds259855.mlab.com:59855/shop',
   SECRET_TOKEN: 'mi@clave@de@tokens@aleato#ria'
 }

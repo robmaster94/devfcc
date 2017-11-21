b998c24ccdb5e430f1826bc766f8951e485a2387
Paco Sanchez <francisco.sanchez.14@gmail.com>
2017-11-21 20:49:53 +0100

    removed ocpp.js controller, not needed. Updated ocpp.js routes and index.js

--- app_api/routes/ocpp.js
+++ app_api/routes/ocpp.js
@@ -1,17 +1,5 @@
 'use strict'
 
-const express = require('express')
-//const auth = require('../middlewares/auth')
-const ocppCtrl = require('../controllers/ocpp')
-const ocppRouter = express.Router()
-
-/*
-  Creo otro express.Router() para crear un servidor WebSocket en un módulo aparte
-  La ruta será /ocpp/wallbox-sn2197, siendo ocpp el express.Router()
-*/
-
-//ocpp.get('/wallbox-sn2197', ocppCtrl.createWebSocketServer)
-
 var contador = 0;
 
 ocppRouter.websocket('/wallbox-sn2197' , (info,cb,next) => {
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 var http = require("http")
-//const ocppCtrl = require('./app_api/controllers/ocpp')
 
 mongoose.connect(config.db, (err,res) => {
   if(err) {
@@ -14,30 +13,5 @@ mongoose.connect(config.db, (err,res) => {
 
   app.listen(config.port, () => {
     console.log(`API REST corriendo en http://localhost:${config.port}`)
-    /*var WebSocketServer = require("ws").Server
-    var port = 8050
-
-    var server = http.createServer(app)
-    server.listen(config.port)
-
-    console.log("http server listening on %d", port)
-
-    var wss = new WebSocketServer({
-        server: server
-    })
-    console.log("websocket server created")
-
-    wss.on("connection", function (ws) {
-        var id = setInterval(function () {
-            ws.send(JSON.stringify(new Date()), function () {})
-        }, 1000)
-
-        console.log("websocket connection open")
-
-        ws.on("close", function () {
-            console.log("websocket connection close")
-            clearInterval(id)
-        })
-    })*/
   })
 })

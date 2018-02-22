fa568de2001faf7d89e7449f858a2c04bf178622
Paco Sanchez <francisco.sanchez.14@gmail.com>
2018-02-22 20:00:01 +0100

    Some minor changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  db: process.env.MONGODB || /*'mongodb://user.paco.dev.fcc:PaCoCrIs02112014@ds113936.mlab.com:13936/futurecharge'*/
-                             'mongodb://localhost:27017/futurecharge',
+  db: process.env.MONGODB || 'mongodb://user.paco.dev.fcc:PaCoCrIs02112014@ds113936.mlab.com:13936/futurecharge'
+                             //'mongodb://localhost:27017/futurecharge',
   SECRET_TOKEN: 'mi@clave@de@tokens@aleato#ria'
 }
